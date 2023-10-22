6d16446063075d542275eaaa844aa85596d4f1c0
Gabriel Farias <gabrielfalves2003@gmail.com>
2023-10-21 22:09:56 -0300

    feat: add function that receives activities array and calc remaining check ins

---
+++ fetchDataAndCalc.js
@@ -0,0 +1,40 @@
+function calcRemainingCheckIns(activitiesArray) {
+    //attendance1
+    //attendance2
+    //attendance3
+
+    // 10 = presente
+    // 0 = falta
+    // -1 = data futura
+
+    let missedCheckIns = 0;
+    let futureCheckIns = 0;
+    let completedCheckIns = 0;
+
+    let classDays = 0;
+
+    activitiesArray.forEach((activity) => {
+        if (!activity.date) return;
+
+        classDays++;
+        for (let i = 1; i <= 3; i++) {
+            const attendance = activity[`attendance${i}`];
+
+            if (attendance === 10) completedCheckIns++;
+            else if (attendance === 0) missedCheckIns++;
+            else if (attendance === -1) futureCheckIns++;
+        }
+    });
+
+    const totalCheckIns = classDays * 3;
+    const totalCheckInsLostToEndWith25 = totalCheckIns * 0.25;
+    const remainingCheckInsToLoose = Math.floor(
+        totalCheckInsLostToEndWith25 - missedCheckIns
+    );
+
+    console.log(
+        "Você ainda pode perder ",
+        remainingCheckInsToLoose,
+        " check-ins"
+    );
+}
